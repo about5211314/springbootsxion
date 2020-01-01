3a8ed7e6329ed85956ea7450dbe8bdec38cddb7f
about5211314 <about5211314@qq.com>
2020-01-01 19:16:46 +0800

    增加跨域功能和数据库文件：resources/data

--- springboot-api/src/main/java/com/api/springbootapi/controller/TestController.java
+++ springboot-api/src/main/java/com/api/springbootapi/controller/TestController.java
@@ -13,6 +13,19 @@ import java.util.Date;
 @RequestMapping("/test")
 public class TestController {
 
+    /**
+     * showdoc
+     * @catalog 测试文档/用户相关
+     * @title 用户登录
+     * @description 用户登录的接口
+     * @method get
+     * @url https://www.showdoc.cc/home/user/login
+     * @header token 可选 string 设备token
+     * @return {"error_code":0,"data":{"uid":"1","username":"12154545","name":"吴系挂","groupid":2,"reg_time":"1436864169","last_login_time":"0"}}
+     * @return_param groupid int 用户组id
+     * @return_param name string 用户昵称
+     * @number 99
+     */
     @GetMapping("/date")
     public String getdate(){
         Date d = CommonMethod.getCurDate();
