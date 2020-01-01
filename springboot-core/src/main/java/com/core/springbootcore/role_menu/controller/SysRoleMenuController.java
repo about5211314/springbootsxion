0f6ca7283d9ccfdd468c68baecb66ff667a5aed8
about5211314 <about5211314@qq.com>
2020-01-01 19:20:23 +0800

    提交新模块：全文检索es

---
+++ springboot-core/src/main/java/com/core/springbootcore/role_menu/controller/SysRoleMenuController.java
@@ -0,0 +1,20 @@
+package com.core.springbootcore.role_menu.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 角色与菜单对应关系 前端控制器
+ * </p>
+ *
+ * @author shenl
+ * @since 2019-12-12
+ */
+@RestController
+@RequestMapping("/role_menu/sys-role-menu")
+public class SysRoleMenuController {
+
+}
---
+++ springboot-core/src/main/java/com/core/springbootcore/role_menu/service/ISysRoleMenuService.java
@@ -0,0 +1,16 @@
+package com.core.springbootcore.role_menu.service;
+
+import com.core.springbootcore.role_menu.entity.SysRoleMenu;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 角色与菜单对应关系 服务类
+ * </p>
+ *
+ * @author shenl
+ * @since 2019-12-12
+ */
+public interface ISysRoleMenuService extends IService<SysRoleMenu> {
+
+}
